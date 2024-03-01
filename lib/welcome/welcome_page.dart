a1b08f19ffaad16f1973d37a028c318b0ab359b7
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-03-01 20:44:27 +0000

    images updates

--- lib/welcome/welcome_page.dart
+++ lib/welcome/welcome_page.dart
@@ -5,6 +5,12 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
   }
 }
