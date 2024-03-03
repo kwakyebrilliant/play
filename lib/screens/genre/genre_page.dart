6376fd820c3d40311a66199d27601d4c2a33b961
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-03-03 17:34:45 +0000

    genrepage added

---
+++ lib/screens/genre/genre_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GenrePage extends StatefulWidget {
+  const GenrePage({super.key});
+
+  @override
+  State<GenrePage> createState() => _GenrePageState();
+}
+
+class _GenrePageState extends State<GenrePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
