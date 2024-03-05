5f7f651d110735534f75b577fc529ef0c3108bb8
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-03-05 06:59:32 +0000

    genre page updates

--- lib/screens/genre/genre_page.dart
+++ lib/screens/genre/genre_page.dart
@@ -710,6 +710,104 @@ class _GenrePageState extends State<GenrePage> {
                     ),
                   ),
                 ),
+
+                //recent searches 4
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFE3E5E5).withOpacity(0.7),
+                      borderRadius: BorderRadius.circular(20.0),
+                      border: Border.all(
+                        width: 1.0,
+                        color: const Color(0xFFF7fAFB),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Row(
+                        children: [
+                          //image
+                          Container(
+                            height: 90.0,
+                            width: 90.0,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20.0),
+                              image: const DecorationImage(
+                                image: AssetImage("assets/images/sing2.jpg"),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+
+                          //song name and artiste name
+                          const Expanded(
+                            flex: 4,
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  //song name
+                                  Text(
+                                    "This is the way of life here ",
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+
+                                  //name of artiste
+                                  Text(
+                                    "brilliant",
+                                    style: TextStyle(
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+
+                          // Spacer to push the favorite icon to the end
+                          const Spacer(),
+
+                          //favorite icon
+                          Column(
+                            children: [
+                              // favorite icon wrapped in a container
+                              Container(
+                                height: 28.0,
+                                width: 28.0,
+                                decoration: BoxDecoration(
+                                  color:
+                                      const Color(0xFFE3E5E5).withOpacity(0.7),
+                                  borderRadius: BorderRadius.circular(60.0),
+                                  border: Border.all(
+                                    width: 1.0,
+                                    color: const Color(0xFFF7fAFB),
+                                  ),
+                                ),
+                                // favorite icon
+                                child: const Icon(
+                                  Icons.favorite_outlined,
+                                  size: 20.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
