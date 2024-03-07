ae9783d8fecc9e434df934c131648288bd2b4c24
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-03-07 11:30:16 +0000

    library updates

--- lib/screens/library/library_page.dart
+++ lib/screens/library/library_page.dart
@@ -71,7 +71,7 @@ class _LibraryPageState extends State<LibraryPage> {
                           ),
                         ),
 
-                        Text("data"),
+                        //search here
                       ],
                     ),
                   ),
