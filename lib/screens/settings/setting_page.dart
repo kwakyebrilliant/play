ff73f7bd30a5543c45ccb4328eb15b4b1db7ac07
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-03-19 09:08:45 +0000

    settings updates

--- lib/screens/settings/setting_page.dart
+++ lib/screens/settings/setting_page.dart
@@ -117,12 +117,12 @@ class _SettingPageState extends State<SettingPage> {
                     children: [
                       //account here
                       Padding(
-                        padding: const EdgeInsets.only(right: 15.0),
+                        padding: const EdgeInsets.only(right: 20.0),
                         child: Container(
                           height: 150.0,
                           width: 150.0,
                           decoration: BoxDecoration(
-                            //color: const Color(0xFFE3E5E5).withOpacity(0.2),
+                            color: const Color(0xFFE3E5E5).withOpacity(0.2),
                             borderRadius: BorderRadius.circular(20.0),
                             border: Border.all(
                               color: const Color(0xFFF7fAFB),
@@ -144,7 +144,7 @@ class _SettingPageState extends State<SettingPage> {
                                     color: const Color(0xFFF7fAFB),
                                   ),
                                 ),
-                                // Play icon
+                                // Person icon
                                 child: const Icon(
                                   Icons.person_rounded,
                                   size: 30.0,
@@ -155,6 +155,56 @@ class _SettingPageState extends State<SettingPage> {
                                 padding: EdgeInsets.only(top: 10.0),
                                 child: Text(
                                   'Account',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      //premium here
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20.0),
+                        child: Container(
+                          height: 150.0,
+                          width: 150.0,
+                          decoration: BoxDecoration(
+                            color: const Color(0xFFE3E5E5).withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(20.0),
+                            border: Border.all(
+                              color: const Color(0xFFF7fAFB),
+                              width: 1.0,
+                            ),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 42.0,
+                                width: 42.0,
+                                decoration: BoxDecoration(
+                                  color:
+                                      const Color(0xFFE3E5E5).withOpacity(0.7),
+                                  borderRadius: BorderRadius.circular(60.0),
+                                  border: Border.all(
+                                    width: 1.0,
+                                    color: const Color(0xFFF7fAFB),
+                                  ),
+                                ),
+                                // workspace premium icon
+                                child: const Icon(
+                                  Icons.workspace_premium_rounded,
+                                  size: 30.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(top: 10.0),
+                                child: Text(
+                                  'Premium',
                                   style: TextStyle(
                                     color: Colors.white,
                                   ),
