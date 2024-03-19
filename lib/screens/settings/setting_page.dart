7e95f90df0272bb0d6ee3dda62c32943e700c4e5
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-03-19 09:25:57 +0000

    settings page updates

--- lib/screens/settings/setting_page.dart
+++ lib/screens/settings/setting_page.dart
@@ -42,51 +42,65 @@ class _SettingPageState extends State<SettingPage> {
             padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 40.0),
             child: Column(
               children: [
-                //profile, greeting and username
+                //profile, greeting, username and notification icon
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    //profile Avatar
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          color: const Color(0xFFF7fAFB),
-                          width: 1.0,
+                    //profile, greeting and username
+                    Row(
+                      children: [
+                        //profile Avatar
+                        Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: const Color(0xFFF7fAFB),
+                              width: 1.0,
+                            ),
+                          ),
+                          child: const CircleAvatar(
+                            radius: 20.0,
+                            backgroundImage:
+                                AssetImage('assets/images/sing1.jpg'),
+                          ),
                         ),
-                      ),
-                      child: const CircleAvatar(
-                        radius: 20.0,
-                        backgroundImage: AssetImage('assets/images/sing1.jpg'),
-                      ),
+
+                        //Greetings and username
+                        Padding(
+                          padding: const EdgeInsets.only(left: 5.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              //Greetings here
+                              Text(
+                                '${_greetUser()},',
+                                style: const TextStyle(
+                                  fontSize: 13.0,
+                                  color: Color(0xFFF7fAFB),
+                                  fontWeight: FontWeight.w300,
+                                ),
+                              ),
+
+                              //username here
+                              const Text(
+                                '@Brilliant',
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  color: Color(0xFFF7fAFB),
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
 
-                    //Greetings and username
-                    Padding(
-                      padding: const EdgeInsets.only(left: 5.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          //Greetings here
-                          Text(
-                            '${_greetUser()},',
-                            style: const TextStyle(
-                              fontSize: 13.0,
-                              color: Color(0xFFF7fAFB),
-                              fontWeight: FontWeight.w300,
-                            ),
-                          ),
-
-                          //username here
-                          const Text(
-                            '@Brilliant',
-                            style: TextStyle(
-                              fontSize: 18.0,
-                              color: Color(0xFFF7fAFB),
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ],
-                      ),
+                    //notification icon
+                    const Icon(
+                      Icons.notifications,
+                      size: 30.0,
+                      color: Colors.white,
                     ),
                   ],
                 ),
@@ -165,6 +179,56 @@ class _SettingPageState extends State<SettingPage> {
                         ),
                       ),
 
+                      //devices here
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20.0),
+                        child: Container(
+                          height: 150.0,
+                          width: 150.0,
+                          decoration: BoxDecoration(
+                            color: const Color(0xFFE3E5E5).withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(20.0),
+                            border: Border.all(
+                              color: const Color(0xFFF7fAFB),
+                              width: 1.0,
+                            ),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 42.0,
+                                width: 42.0,
+                                decoration: BoxDecoration(
+                                  color:
+                                      const Color(0xFFE3E5E5).withOpacity(0.7),
+                                  borderRadius: BorderRadius.circular(60.0),
+                                  border: Border.all(
+                                    width: 1.0,
+                                    color: const Color(0xFFF7fAFB),
+                                  ),
+                                ),
+                                // devices icon
+                                child: const Icon(
+                                  Icons.devices_rounded,
+                                  size: 30.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(top: 10.0),
+                                child: Text(
+                                  'Devices',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+
                       //premium here
                       Padding(
                         padding: const EdgeInsets.only(right: 20.0),
@@ -212,6 +276,24 @@ class _SettingPageState extends State<SettingPage> {
                               ),
                             ],
                           ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                //Others header text
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Others',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Color(0xFFF7fAFB),
+                          fontWeight: FontWeight.w600,
                         ),
                       ),
                     ],
