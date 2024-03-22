dea0033bd5c9dbb78ee8fc65f1976b82a04fdd6f
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-03-22 10:12:50 +0000

    home updates

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -22,6 +22,46 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF5d7075),
+        elevation: 0,
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Play text
+            const Text(
+              'PLAY',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.w800,
+                color: Color(0xFFF7fAFB),
+              ),
+            ),
+
+            // Play icon wrapped in a container
+            Container(
+              height: 28.0,
+              width: 28.0,
+              decoration: BoxDecoration(
+                color: const Color(0xFFE3E5E5).withOpacity(0.7),
+                borderRadius: BorderRadius.circular(60.0),
+                border: Border.all(
+                  width: 1.0,
+                  color: const Color(0xFFF7fAFB),
+                ),
+              ),
+              // Play icon
+              child: const Icon(
+                Icons.play_arrow_rounded,
+                size: 20.0,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
@@ -38,7 +78,7 @@ class _HomePageState extends State<HomePage> {
         ),
         child: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 40.0),
+            padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 15.0),
             child: Column(
               children: [
                 //profile, greeting and username
@@ -66,7 +106,7 @@ class _HomePageState extends State<HomePage> {
 
                               //username here
                               const Text(
-                                '@Brilliant',
+                                '@Kezia',
                                 style: TextStyle(
                                   fontSize: 20.0,
                                   color: Color(0xFFF7fAFB),
