312f658b0aef582ad3e3a1db9e37a378d5b3e7f8
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-03-27 18:06:28 +0000

    updates

--- lib/screens/library/library_page.dart
+++ lib/screens/library/library_page.dart
@@ -16,7 +16,7 @@ class _LibraryPageState extends State<LibraryPage> {
 
   // Define tab contents
   final List<Widget> _tabContents = [
-    //Recents content
+    //Recents contents
     Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
